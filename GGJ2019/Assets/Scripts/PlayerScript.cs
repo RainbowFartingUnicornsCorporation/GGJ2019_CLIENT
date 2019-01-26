159ba53e3f77d12fb4cb6ba429f6159b4e0b1068
Maxipanda <meunier.guillaume8@gmail.com>
2019-01-26 04:21:49 +0100

    PlayerPrefab

---
+++ GGJ2019/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+	public TextMesh textmesh;
+
+	int food = 100;
+
+	// Use this for initialization
+	void Start () { // Get data from initialization
+		textmesh.text = "PlayerNameSetted";
+	}
+
+
+	void MoveTo (float x, float y) {
+		transform.position = new Vector3 (x, y, 0);
+	}
+
+	void updatePlayerName(string PlayerName){
+		
+
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
