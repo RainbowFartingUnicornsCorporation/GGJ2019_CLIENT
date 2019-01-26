d61da636e1702343a40256a3e84008996a258d8d
warrows <warrows@gamer666.fr>
2019-01-26 05:08:30 +0100

    Main menu starts and stops a server

---
+++ GGJ2019/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+    public ServerHandler serverHandler;
+
+    public void StartAloneButtonPressed()
+    {
+        Debug.Log("Button alone was pressed!");
+        serverHandler.StartServer();
+    }
+
+    public void StartHostingButtonPressed()
+    {
+        Debug.Log("Button host was pressed!");
+        serverHandler.StartServer();
+    }
+
+    public void JoinServerButtonPressed()
+    {
+        Debug.Log("Button join was pressed!");
+    }
+}
---
+++ GGJ2019/Assets/Scripts/ServerHandler.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class ServerHandler : MonoBehaviour
+{
+    private System.Diagnostics.Process serverProcess;
+
+    public void StartServer()
+    {
+        if (serverProcess != null) return;
+        string filePath = System.IO.Path.Combine(Application.streamingAssetsPath, "server.exe");
+        System.Diagnostics.ProcessStartInfo info = new System.Diagnostics.ProcessStartInfo(filePath);
+        info.CreateNoWindow = true;
+        info.UseShellExecute = false;
+        serverProcess = System.Diagnostics.Process.Start(info);
+        Debug.Log("Server started");
+    }
+
+    public void StopServer()
+    {
+        if (serverProcess == null || serverProcess.HasExited) return; 
+        serverProcess.Kill();
+        Debug.Log("Server stopped");
+    }
+
+    void OnApplicationQuit()
+    {
+        StopServer();
+    }
+}
