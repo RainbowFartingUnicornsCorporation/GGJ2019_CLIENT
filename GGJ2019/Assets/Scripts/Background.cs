16fe67f24c4449ef6e4a8e79958f5706e6e5e4dd
warrows <warrows@gamer666.fr>
2019-01-26 22:14:17 +0100

    Add background

---
+++ GGJ2019/Assets/Scripts/Background.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class Background : MonoBehaviour {
+
+    private static bool copy = true;
+
+    private SpriteRenderer[][] sprites;
+
+	void Start ()
+    {
+        if (!copy) return;
+
+        copy = false;
+        SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+        //sprites = new SpriteRenderer[11][7]();
+        for (int i = -5; i <= 5; i++)
+            for (int j = -3; i <= 3; j++)
+            {
+                SpriteRenderer obj = (SpriteRenderer) Object.Instantiate(sprite);
+                obj.transform.Translate(new Vector3(obj.size.x / 2 + i * obj.size.x, obj.size.y / 2 + j * obj.size.y, 0));
+            }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
