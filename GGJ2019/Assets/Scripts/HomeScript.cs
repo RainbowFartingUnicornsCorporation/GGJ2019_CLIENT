95736516e5bd3c9c5a0c9a2cdf87e1f2b9ff8077
warrows <warrows@gamer666.fr>
2019-01-27 16:12:01 +0100

    Update server

--- GGJ2019/Assets/Scripts/HomeScript.cs
+++ GGJ2019/Assets/Scripts/HomeScript.cs
@@ -1,8 +1,12 @@
 ﻿using UnityEngine;
 
-public class HomeScript : MonoBehaviour {
+public class HomeScript : MonoBehaviour
+{
+    SpriteRenderer spriteRenderer;
+    private static Sprite[] sprites;
+    private int spriteId;
 
-	public int food;
+    public int food;
 	public int ressources;
 	public int population;
 	public int foodGoal;
@@ -12,11 +16,20 @@ public class HomeScript : MonoBehaviour {
 	public GameObject ressourcesNum;
 	public GameObject populationNum;
 	public GameObject reservePopNum;
+    
+	void Start ()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteId = 0;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+        if (sprites == null)
+        {
+            sprites = new Sprite[3];
+            sprites[0] = Resources.Load<Sprite>("Sprites/Home_1");
+            sprites[1] = Resources.Load<Sprite>("Sprites/Home_2");
+            sprites[2] = Resources.Load<Sprite>("Sprites/Home_3");
+        }
+    }
 
 	public void UpdateHome(Home homeRef) {
 
@@ -32,4 +45,16 @@ public class HomeScript : MonoBehaviour {
 		reservePopNum.GetComponent<TextMesh> ().text = reservePop + " worker idle";
 
 	}
+
+    void OnGUI()
+    {
+        if (spriteId == 0 && population > 0 ||
+            spriteId == 1 && population > 5 ||
+            spriteId == 2 && population > 10)
+        {
+            spriteRenderer.sprite = sprites[spriteId];
+            GetComponent<Animator>().GetComponent<GameObject>().SetActive(false);
+            spriteId++;
+        }
+    }
 }
