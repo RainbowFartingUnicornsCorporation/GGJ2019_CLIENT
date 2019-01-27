bc11ecd4b00b8c6681c37554936db099d0691897
warrows <warrows@gamer666.fr>
2019-01-27 13:57:51 +0100

    Add animated title (menu)

---
+++ GGJ2019/Assets/Scripts/Title.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Title : MonoBehaviour {
+    private Sprite[] spritesAsset;
+    private Image image;
+    private int counter;
+
+    void Start()
+    {
+        image = GetComponent<Image>();
+        spritesAsset = new Sprite[2];
+        spritesAsset[0] = Resources.Load<Sprite>("Sprites/Title_0");
+        spritesAsset[1] = Resources.Load<Sprite>("Sprites/Title_1");
+        counter = 0;
+    }
+
+    void Update()
+    {
+        counter++;
+        counter %= 40;
+        if (counter < 20)
+            image.sprite = spritesAsset[0];
+        else
+            image.sprite = spritesAsset[1];
+    }
+}
